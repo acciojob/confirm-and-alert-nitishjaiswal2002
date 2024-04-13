e0ec4d17cf40406b939080342c8c5fff55757fd4
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-13 16:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let proceed=prompt("Do you want to proceed");
+if(proceed=='ok'){
+	alert('You clicked OK. Proceeding...');
+}else{
+	alert('You clicked cancel. Exiting... ');
+}
